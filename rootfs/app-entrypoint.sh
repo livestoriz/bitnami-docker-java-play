b7303dbc560fb030c9f75fa97429af80a890960e
dgonzalezruiz <davidgonzalezruiz@ip-192-168-200-66.ec2.internal>
2016-10-28 12:47:08 +0200

    Fix typo in entrypoint

--- rootfs/app-entrypoint.sh
+++ rootfs/app-entrypoint.sh
@@ -13,7 +13,7 @@ log () {
     echo -e "\033[0;33m$(date "+%H:%M:%S")\033[0;37m ==> $1."
 }
 
-if [ "$1" == "activator" -a "$5" == "~run" ] ; then
+if [ "$1" == "activator" -a "$4" == "~run" ] ; then
     if [ ! -d $PROJECT_DIRECTORY ] ; then
       log "Creating example Play application"
       nami execute activator createProject --force $PLAY_PROJECT_NAME $PLAY_TEMPLATE
